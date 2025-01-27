eac0cc25e3524b9daf0b56dac8e2f8baf7d4b14c
grungus-dev <acksosievers@gmail.com>
2025-01-27 04:49:52 -0700

    Added tableTranslation.py

---
+++ Analytics/tableTranslation.py
@@ -0,0 +1,10 @@
+""" 
+Author: Jackson
+
+This file will be used to translate Demland's tables into SQL queries 
+that we can then put into our database
+
+"""
+
+def main():
+    print("Hello world!")
