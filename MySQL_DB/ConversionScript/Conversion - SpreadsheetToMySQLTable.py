65c238e4e2fded93b205b0b111f3e5f1a424e34c
grungus-dev <acksosievers@gmail.com>
2025-02-10 04:13:37 -0700

    Tweaked ConversionScript to accomodate new Spreadsheet

--- MySQL_DB/ConversionScript/Conversion - SpreadsheetToMySQLTable.py
+++ MySQL_DB/ConversionScript/Conversion - SpreadsheetToMySQLTable.py
@@ -1,24 +1,38 @@
 import pandas as pd
 import mysql.connector as mysql
+import os
 
 # 1. Load the spreadsheet.
-file_path = '/absolute/path/to/spreadsheet.xlsx'
-sheet_name = 'Sheet1'   # Sheet name (currently default).
+current_dir = os.getcwd()                                          # Gets the current working directory
+file_path = f'{current_dir}/Tables/on-ground+online_tables.xlsx'   # retrieves the table from the repository
+sheet_name = 'on-ground'   # Sheet name (currently default) [either will be on-ground or online].
 
 df = pd.read_excel(file_path, sheet_name, dtype=str)
 
+""" 
+IF YOU ARE USING THIS SCRIPT TO TRANSLATE THE TABLE FOR YOUR SYSTEM,
+
+HERE ARE THE ONLY VARIABLES THAT YOU NEED TO CHANGE TO GET IT TO WORK:
+
+line 27: user="user"
+line 28: password="password"
+line 29: database="database" 
+
+Read the comments for more information about everything else.
+"""
+
 # 2. Connect to MySQL.
 db = mysql.connect(
-    host="localhost",   # Currently default (will work as-is). Either replace or optionally add a specific port if not on the default 3306.
-    user="user",    # Replace with your MySQL username.
-    password="password",   # Replace with your MySQL password.
-    database="database"    # Replace with your MySQL database name.
+    host="localhost",     # Currently default (will work as-is). Either replace or optionally add a specific port if not on the default 3306.
+    user="user",          # Replace with your MySQL username [default is root].
+    password="password",  # Replace with your MySQL password.
+    database="database"   # Replace with your MySQL database name.
 )
 
 cursor = db.cursor()
 
 # 3. Create table if it doesn't exist.
-table_name = 'CourseData'
+table_name = 'onground'     # < set to either onground or online (depends on what you chose above)
 columns = ", ".join([f"{col} VARCHAR(255)" for col in df.columns])  # Assuming all columns as VARCHAR.
 
 create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});"
---
+++ Tables/README.md
@@ -0,0 +1,16 @@
+# TABLES
+Provided by Professor Demland
+
+Edited by Jackson
+
+# NOTES
+These tables are the cumulative data of various tables given to us by Demland. 
+
+- There are 2 sheets in this document, one for on-ground classes, and one for online classes.  
+	- Toggle between them at the bottom
+- All empty cells have been replaced with "--" to cooperate with the conversion script
+	- if this is ever an issue you can use ctrl+h and replace "--" with nothing
+- I got rid of as many duplicate rows as I could, but its possible that there are some that persist
+
+You shouldn't need to edit this, its simply here so that everyone has it.
+
