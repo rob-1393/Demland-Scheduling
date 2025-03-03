2652d9c159076939b267e913a258866a3d7a97e6
grungus-dev <acksosievers@gmail.com>
2025-03-03 12:02:49 -0700

    Formatting changes to comments and code

--- Analytics/analytics.py
+++ Analytics/analytics.py
@@ -8,22 +8,23 @@ Author: jackson sievers
 import os
 import pandas as pd
 
-def getExcelSheet():
-    current_directory = os.getcwd() # Gets the current working directory
-    table_path = f'{current_directory}/Tables/on-ground+online_tables_[TestTable].xlsx'   # retrieves the table
+def get_excel_sheet():
+    current_directory = os.getcwd()
+    table_path = f'{current_directory}/Tables/on-ground+online_tables_[TestTable].xlsx'   # retrieves excel sheet of test data
     sheet_name = 'on-ground' # Sheet name (currently default) [either will be "on-ground" or "online"].
 
-    excel_data = pd.read_excel(table_path, sheet_name)
+    excel_data = pd.read_excel(table_path, sheet_name, dtype=str)
 
     return excel_data
 
 def main():
-    excel_data = getExcelSheet()
+    excel_data = get_excel_sheet()
 
     print(excel_data)
 
+main()
 
-# process needed:
+# PROCESSES NEEDED:
 #
 # Read the data
 # Look at previous dates
@@ -39,12 +40,11 @@ def main():
 # Convert to XML
 #   Reformat data to format below (what i view to be the most important data)
 
-# Export an XML document that looks something like this:
+# Export an XML document that's formatted something like this:
 # | ClassStart | ClassEnd  | Course  | Section | ClassSchedDescription (name) | Professor    | Days | StartTime   | EndTime     | Cr (credits) | Max (students) |
 # | 5/6/2024   | 6/23/2024 | ITT-307 | TR1100A | Cybersecurity Foundations    | Albert Kelly | W F  | 11:00:00 AM | 12:45:00 PM | 4            | 32             |
+
 
 # NOTES:
 # unsure if i want to modify the data given directly from the excel sheet, or insert each class into another "database"
 # may conduct analysis in python and export to other database for storage
-
-main()
