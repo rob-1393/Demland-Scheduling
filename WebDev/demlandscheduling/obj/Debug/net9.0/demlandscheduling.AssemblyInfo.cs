9f759d8a36eab9d6acea6c2b11a3e49789413c32
rob-1393 <anemailbeingused@gmail.com>
2025-03-17 16:12:47 -0700

    Added calculation of start and end dates (YYYY-MM). Currently includes past and present semesters.

--- WebDev/demlandscheduling/obj/Debug/net9.0/demlandscheduling.AssemblyInfo.cs
+++ WebDev/demlandscheduling/obj/Debug/net9.0/demlandscheduling.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demlandscheduling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a885614bf67c6b7677ff60707ea524f7da0ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0925f18afadaa574a2e9918d4abd10719c7d67e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("demlandscheduling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demlandscheduling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
