b87d26cfad1701982454b3ece65a32accc3ca6c3
rob-1393 <62722044+rob-1393@users.noreply.github.com>
2025-03-25 19:27:38 -0700

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 8e8a845063c5f2e1ef5c8c38d5c0924169b4d71a, reversing
    changes made to b3879d843605f6b0511373d2bdd71bc3770c0f76.

--- WebDev/demlandscheduling/Pages/Index.cshtml.cs
+++ WebDev/demlandscheduling/Pages/Index.cshtml.cs
@@ -3,31 +3,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using demlandscheduling.Data;
 using demlandscheduling.DataModels;
-// The above proivdes the necessary components for the Razor Pages, where Microsoft.EntityFrameworkCoore allows interactions within the database.
-// While demlandscheduling.Data and demlandscheduling.DataModels is declared to allow the specific database and model classes to be used for the following code.
 
-// Below the PageModel is declared to be part of the Razor Pages Framework.
-// Meanwhile, the ApplicationDbContext is responsible for database interactions to be made. Lastly, the _Context stores the database context.
-namespace demlandscheduling.Pages.Index  
+namespace demlandscheduling.Pages.Index
 {
     public class IndexModel(ApplicationDbContext context) : PageModel
     {
         private readonly ApplicationDbContext _context = context;
 
-        public required IList<DemlandData> DemlandDataList { get; set; }  // Required to store a list of DemlandData items that are obtained from the database
+        public required IList<DemlandData> DemlandDataList { get; set; }  // No need for nullable if we ensure it is initialized
 
         public async Task OnGetAsync()
         {
-            // The OnGetAsync data runs when the page is loaded through an HTTP request.
-            // It will then proceed with querying the DemlandData table in a asynchronously process. Which lastly, the ToListAsync will then convert the query results into a list.
+            // Fetch data from the 'DemlandData' table in SQL Server
             DemlandDataList = await _context.DemlandData.ToListAsync();
 
-            // If the DemlandDataList is seen as null, it gets intialized with an empty list
+            // Ensure DemlandDataList is never null
             if (DemlandDataList == null)
             {
-                DemlandDataList = new List<DemlandData>();
+                DemlandDataList = new List<DemlandData>();  // Initialize with an empty list if null
             }
         }
     }
 }
-// In Summary the following coding fetches and stores the DemlandData from the database in a asynchronously style, while also ensuring the DemlandDataList properties are intialized properly.
